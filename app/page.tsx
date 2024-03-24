67d69930fdd9225f85dc8664ce7307e29b5f54e1
Marcos Barrera <marcos.barrera@colorado.edu>
2024-03-23 17:25:30 -0700

    Refactor code to configure Amplify and add sign out functionality
    - Configure Amplify using the provided configuration file.
    - Add import statement for `Amplify` from `'aws-amplify'`.
    - Update function signature of `App` component to include `signOut` and `user` props.
    - Display the username in the greeting message.
    - Add a button to allow users to sign out.
    
    The changes aim to enhance user experience by providing sign out functionality and personalizing the greeting message with the user's username.

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,18 @@
 // app/page.tsx
 "use client";
-
+import type { AppProps } from "next/app";
 import { withAuthenticator } from "@aws-amplify/ui-react";
+import { Amplify } from 'aws-amplify';
+import config from '@/amplifyconfiguration.json';
 import "@aws-amplify/ui-react/styles.css";
+import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
 
-function App() {
+Amplify.configure(config);
+function App({signOut, user}: WithAuthenticatorProps & AppProps) {
   return (
       <>
-        <h1>Hello, Amplify 👋</h1>
+          <h1>Hello, {user?.username} 👋</h1>
+          <button onClick={signOut}>Sign out</button>
       </>
   );
 }
